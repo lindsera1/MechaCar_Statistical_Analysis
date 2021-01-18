5980be5831ecb91184a5a1b895c795555cda4ad6
lindsera1 <52899223+lindsera1@users.noreply.github.com>
2021-01-17 23:10:07 -0600

    uploads

---
+++ MechaCarChallenge.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+MechaCarDF <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+Suspension_Coil_Table <- read.csv(file="Suspension_Coil.csv", header=T, sep=",")
+
+total_summary <- Suspension_Coil_Table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups=('keep'))
+
+lot_summary <- Suspension_Coil_Table %>% group_by(Manufacturing_Lot) %>%summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups=('keep'))
