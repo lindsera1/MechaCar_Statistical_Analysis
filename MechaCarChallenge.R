818f4565fbab9a19b4f2e7c54db5c7f3cc95a2ad
lindsera1 <52899223+lindsera1@users.noreply.github.com>
2021-01-18 00:44:01 -0600

    final

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,6 @@ Suspension_Coil_Table <- read.csv(file="Suspension_Coil.csv", header=T, sep=",")
 total_summary <- Suspension_Coil_Table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups=('keep'))
 
 lot_summary <- Suspension_Coil_Table %>% group_by(Manufacturing_Lot) %>%summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups=('keep'))
+
+t.test(Suspension_Coil_Table$PSI,mu=(1500)) #compare sample versus population means
+
