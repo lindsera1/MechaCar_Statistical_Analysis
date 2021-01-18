b9b14022715d069ddc9a6a50f4919dcb9518a9aa
lindsera1 <52899223+lindsera1@users.noreply.github.com>
2021-01-18 00:58:56 -0600

    uploads

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,14 @@ lot_summary <- Suspension_Coil_Table %>% group_by(Manufacturing_Lot) %>%summariz
 
 t.test(Suspension_Coil_Table$PSI,mu=(1500)) #compare sample versus population means
 
+var_a <- Suspension_Coil_Table$PSI[Suspension_Coil_Table$Manufacturing_Lot == "Lot1"]
+
+var_b <- Suspension_Coil_Table$PSI[Suspension_Coil_Table$Manufacturing_Lot == "Lot2"]
+
+var_c <- Suspension_Coil_Table$PSI[Suspension_Coil_Table$Manufacturing_Lot == "Lot3"]
+
+t.test(var_a,mu=(1500))
+
+t.test(var_b,mu=(1500))
+
+t.test(var_c, mu=(1500))
